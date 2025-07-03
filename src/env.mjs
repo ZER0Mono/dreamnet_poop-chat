c9e466730fd459b74aa446b54c45643637e57a78
ZER0Mono <zero@monoclub.io>
2025-07-03 14:15:54 +0200

    env.mjs fix for deploy

--- src/env.mjs
+++ src/env.mjs
@@ -1,9 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    APP_URL: z.string().url().min(1),
     // DATABASE_URL: z.string().min(1),
-    // APP_URL: z.string().url().min(1),
     // GOOGLE_SITE_VERIFICATION_ID: z.string().optional(),
     // GITHUB_ID: z.string().min(1),
     // GITHUB_SECRET: z.string().min(1),
@@ -17,8 +18,8 @@ export const env = createEnv({
     // NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
+    APP_URL: process.env.APP_URL,
     // DATABASE_URL: process.env.DATABASE_URL,
-    // APP_URL: process.env.APP_URL,
     // GOOGLE_SITE_VERIFICATION_ID: process.env.GOOGLE_SITE_VERIFICATION_ID,
     // GITHUB_ID: process.env.GITHUB_ID,
     // GITHUB_SECRET: process.env.GITHUB_SECRET,
